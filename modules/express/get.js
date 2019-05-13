dc430b55cde4f4016f362ab3c37b25f9893eeaa1
Alexander Markgraf <alexander.markgraf@secmaker.com>
2019-05-13 17:03:53 +0200

    "Improved" status messages

--- modules/express/get.js
+++ modules/express/get.js
@@ -3,6 +3,14 @@ exports.getStatus = (discordStatus) => {
         // var1 = botstatus
         let random = Math.random() * 10;
         translateStatus = "Unknown";
+
+        items = [
+            "I'm a teapot",
+            "If you have found a lost Harvi, please return him to the nearest ASE moderator",
+            "Minions. Minions everywhere",
+            "Karlsson says hi",
+            "The Arma 3 map of Altis is based on the real-life Greek island called Lemnos, while the map Stratis is based on the island called Agios Efstratios"
+        ]
     
         switch (discordStatus) {
             case 0: 
@@ -20,16 +28,24 @@ exports.getStatus = (discordStatus) => {
             case 4:
                 translateStatus = "Nearly";
                 break;
-            case 4:
+            case 5:
                 translateStatus = "Disconnected";
                 break;
             default:
                 translateStatus = "Unknown, check process status";
         }
         expressStatus = 200;
-        if (random > 8) {
-            expressStatus = "418: I'm a teapot";
+        message = "";
+        if (random > 9) {
+            message = items[Math.floor(Math.random()*items.length)];
+            if (message == items[0]) {
+                expressStatus = 418;
+            }
         }
-    
-        return {"expressStatus": expressStatus, "discordStatus": translateStatus};
+
+        if (random > 9) {
+            return {"express": expressStatus, "discord": translateStatus, "message": message};
+        } else {
+            return {"express": expressStatus, "discord": translateStatus};
+        }
 }
