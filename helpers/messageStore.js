42f96414456f5e9d21a55aa203929fb81155245f
Alex M <alex@obliv1on.com>
2019-09-14 18:43:04 +0200

    Add functionality to allow reading of multiple channels from one guild

--- helpers/messageStore.js
+++ helpers/messageStore.js
@@ -2,13 +2,21 @@ exports.convertMessage = (message) => {
     // authorObject = id, name, avatarURL
     // contentObject = time, url, string, attachments
     let config = require('../json/conf.json');
-    if (message.channel.id == config.getMessagesFrom) {
+    if (message.guild.id == config.getMessagesFrom) {
         let authorObject = {
             id: message.author.id,
             name: message.author.tag,
             avatarURL: message.author.avatarURL
         }
-        contentObject = {
+        let contentObject = {
+            guild: {
+                id: message.guild.id,
+                name: message.guild.name
+            },
+            channel: {
+                id: message.channel.id,
+                name: message.channel.name
+            },
             time: message.createdTimestamp,
             url: message.url,
             id: message.id,
